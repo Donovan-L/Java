1e8f18ca9c7fd4e909f3b5ac1efa2266dfb643e5
Donovan-L <donovan_lemmon@hotmail.com>
2016-09-26 08:02:43 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+// save this file in a folder called java you have create in home
+// Hello.java
+// compile with javac
+class Hello {
+	public static void main (String a[]){
+		System.out.println("Hello world!");
+	}
+}
