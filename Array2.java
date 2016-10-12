7c40ea1880d686ab303d17129ab7851627bd8dc9
Donovan-L <donovan_lemmon@hotmail.com>
2016-10-12 08:38:24 -0700

    Create Array2.java

---
+++ Array2.java
@@ -0,0 +1,10 @@
+import java.until.Arrays;
+class Array2 {
+  public stsatic void printArray(int[] iarray) {
+    int length = iarray.length;
+    int i;
+      for (i = 0; i < length; i++){
+        System.out.print(iarray[i]);
+        System.out.print(" . ");
+     }
+  }
